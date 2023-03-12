995febf166e2a59e49f53b6030acce41993e7107
Jas <jaswithareddy@uchicago.edu>
2023-03-12 00:52:23 -0600

    Minor Changes

--- Shall We Play A Game?/GridView.swift
+++ Shall We Play A Game?/GridView.swift
@@ -9,25 +9,28 @@ import Foundation
 import UIKit
 
 class GridView: UIView {
+    
+    /// drawing the grid
+    
     override func draw(_ rect: CGRect) {
 
         let path = UIBezierPath()
         
-        // Two vertical lines
-        path.move(to: CGPoint(x: 100, y: 0))
-        path.addLine(to: CGPoint(x: 100, y: 300))
-        path.move(to: CGPoint(x: 200, y: 0))
-        path.addLine(to: CGPoint(x: 200, y: 300))
+        // setting color of the grid
+        UIColor.purple.setStroke()
+        path.lineWidth = 5
+        path.stroke()
         
-        // Two horizontal lines
+        // two horizontal lines
         path.move(to: CGPoint(x: 0, y: 100))
         path.addLine(to: CGPoint(x: 300, y: 100))
         path.move(to: CGPoint(x: 0, y: 200))
         path.addLine(to: CGPoint(x: 300, y: 200))
         
-        // Set color of the lines
-        UIColor.purple.setStroke()
-        path.lineWidth = 5
-        path.stroke()
+        // two vertical lines
+        path.move(to: CGPoint(x: 100, y: 0))
+        path.addLine(to: CGPoint(x: 100, y: 300))
+        path.move(to: CGPoint(x: 200, y: 0))
+        path.addLine(to: CGPoint(x: 200, y: 300))
     }
 }
